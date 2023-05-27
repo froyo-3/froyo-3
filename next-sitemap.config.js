98f13730d0a6430a9d45cd9c1e7da90430639823
Zirpluu <119133338+Zirpluu@users.noreply.github.com>
2023-05-27 15:45:50 +0000

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://zirpluu.ml',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
