4ac3f7b61efb393b12fe2eae43571b265bf68339
Zirpluu <emiltye@gmail.com>
2023-06-01 19:50:17 +0100

    more text on project cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,39 +44,39 @@ const Home: NextPage = () => {
                 <h4 className="italic font-mono">reactJS</h4>
                 <h3 className="text-2xl font-sans font-bold">emiltye.ml</h3>
                 <p className="font-mono py-2"> the website I used to first learn react</p>
-                <p className="font-mono">Emiltye.ml was the website I used to learn React. Thanks to this resource, I gained the confidence and skills to develop complex React applications.</p>
+                <p className="font-mono">Emiltye.ml was a valuable website for me to learn React. It provided me with the necessary resources and guidance to enhance my skills and tackle complex React application development. Thanks to Emiltye.ml, I gained the confidence and knowledge required to build advanced React applications successfully.</p>
               </div>
 
               <div className="border-b-2 border-x p-5 rounded-xl flex-shrink-0 snap-center max-w-[40vh] min-w-[40vh]">
                 <h4 className="italic font-mono">Python</h4>
                 <h3 className="text-2xl font-sans font-bold">Word Generator</h3>
                 <p className="font-mono py-2"> a python script which outputs random words </p>
-                <p className="font-mono">
-I developed a Python word generator program that efficiently generates unique and creative words. it is a versatile tool for naming characters, inventing product names, and inspiring creative writing.</p>
+                <p className="font-mono">I created a Python word generator program that efficiently produces distinct and imaginative words. It serves as a versatile tool for character naming, inventing product names, and sparking creativity in writing endeavors.</p>
               </div>
 
               <div className="border-b-2 border-x p-5 rounded-xl flex-shrink-0 snap-center max-w-[40vh] min-w-[40vh]">
                 <h4 className="italic font-mono">Python</h4>
                 <h3 className="text-2xl font-sans font-bold">Login Page</h3>
-                <p className="font-mono">a login page made in python</p>
+                <p className="font-mono py-2">a login page made in python</p>
+                <p className="font-mono">I created a Python-based login page. Through a user-friendly interface, this login page prompts users to input their username and password securely. The login page then verifies the entered information against the stored credentials, granting access only to authorized users.</p>
               </div>
-
               <div className="border-b-2 border-x p-5 rounded-xl flex-shrink-0 snap-center max-w-[40vh] min-w-[40vh]">
                 <h4 className="italic font-mono">Python</h4>
                 <h3 className="text-2xl font-sans font-bold">Community Calculator</h3>
-                <p className="font-mono">answers questions based on what it knows</p>
+                <p className="font-mono py-2">answers questions based on what it knows</p>
+                <p className="font-mono"> I created an advanced calculator in Python that can do more than basic arithmetic. It can remember previous answers and use them in new calculations. This feature allows users to reference and use the results of previous equations, making it easier and faster to perform complex mathematical operations.</p>
               </div>
 
               <div className="border-b-2 border-x p-5 rounded-xl flex-shrink-0 snap-center max-w-[40vh] min-w-[40vh]">
                 <h4 className="italic font-mono">Python</h4>
                 <h3 className="text-2xl font-sans font-bold">Project</h3>
-                <p className="font-mono">Lorem ipsum dolor sit amet.</p>
+                <p className="font-mono">Lorem ipsum dolor sit amet. In nemo odio nam minima eveniet eum voluptas illum et quos nemo aut iusto esse At omnis dolorem aut earum architecto! Non quae itaque ut nulla quam ut cumque sunt. Ea sint aperiam 33 similique fugit qui laboriosam iure qui itaque consequatur cum consequatur quod est molestiae consequuntur. Et cumque dolorem et ipsam dolore et atque voluptas.</p>
               </div>
 
               <div className="border-b-2 border-x p-5 rounded-xl flex-shrink-0 snap-center max-w-[40vh] min-w-[40vh]">
                 <h4 className="italic font-mono">Python</h4>
                 <h3 className="text-2xl font-sans font-bold">Project</h3>
-                <p className="font-mono">Eos soluta maiores est commodi odio aut placeat ratione.</p>
+                <p className="font-mono">Lorem ipsum dolor sit amet. Aut consequatur assumenda ad quia eaque non omnis repellat ut veritatis autem. Et inventore fugit aut voluptatem recusandae sit rerum perferendis non enim mollitia ut excepturi quia. Aut quia quidem eum nulla provident non consequatur autem et molestiae veniam sit saepe atque sed blanditiis mollitia. Ut molestias magni qui alias natus qui alias veritatis nam temporibus culpa ut modi velit!</p>
               </div>
           </div>
         </div>
